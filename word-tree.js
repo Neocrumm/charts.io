0d780d5ed1045f98ba34dd522f531f91a5277e24
parker <karasolomon2010@gmail.com>
2021-02-21 08:05:55 -0800

    Update word-tree.js

--- word-tree.js
+++ word-tree.js
@@ -4,31 +4,19 @@
       function drawChart() {
         var data = google.visualization.arrayToDataTable(
           [ ['Phrases'],
-            ['cats are better than dogs'],
-            ['cats eat kibble'],
-            ['cats are better than hamsters'],
-            ['cats are awesome'],
-            ['cats are people too'],
-            ['cats eat mice'],
-            ['cats meowing'],
-            ['cats in the cradle'],
-            ['cats eat mice'],
-            ['cats in the cradle lyrics'],
-            ['cats eat kibble'],
-            ['cats for adoption'],
-            ['cats are family'],
-            ['cats eat mice'],
-            ['cats are better than kittens'],
-            ['cats are evil'],
-            ['cats are weird'],
-            ['cats eat mice'],
+           ['coding is fun']
+           ['coding is also called programming']
+           ['coding is done by programmers']
+           ['coding is cool']
+           ['coding will may be inproved in 50 years']
+           ['coding has two forms, block-based and text-based']
           ]
         );
 
         var options = {
           wordtree: {
             format: 'implicit',
-            word: 'cats'
+            word: 'coding'
           }
         };
 
